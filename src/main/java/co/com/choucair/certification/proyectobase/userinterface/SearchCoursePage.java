c5e5f21ed079e3f15c689a563b98a38b4af2f600
landreaguti <36176855+landreaguti@users.noreply.github.com>
2021-08-10 17:12:22 -0500

    Se corrige

--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 
-public class SearchCoursePage extends PageObject {
+public class SearchCoursePage {
     public static final Target BUTTON_UC = Target.the( "selecciona la universidad choucair").located(By.xpath("//div[@id='universodad']//strong"));
     public static final Target INPUT_COURSE = Target.the("Buscar el curso").located(By.id("coursesearchbox"));
     public static final Target BUTTON_GO = Target.the("Da click para buscar el curso").located(By.id("//button[@class='btn btn-secondary']"));
